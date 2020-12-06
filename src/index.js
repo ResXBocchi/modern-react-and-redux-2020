9581a06b81ffe85f9e69ce3e3b8eee8d402fee28
Renan Bocchi <67618681+ResXBocchi@users.noreply.github.com>
2020-12-06 11:42:01 -0300

    updating

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+// Import React and ReactDOM
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// could be: const React = require('react');
-// could be: const ReactDOM = require('react-dom');
+//// could be: const React = require('react');
+//// could be: const ReactDOM = require('react-dom');
 
+function getButtonText(){
+    return 'Click on me!'
+}//// could be: const buttonText = 'Click me!' then pass the variable inside the button tag.
+
+// Create a react component
+const App = () => {
+
+    return (
+        <div>
+            <label className='label' for='name'>Enter name: </label>
+            <input id='name' type='text' />
+    <button style={{ backgroundColor:'blue',color:'white' }}>{getButtonText()}</button>
+        </div>
+    )
+};
+
+//// could be: const App = function(){return <div>Hi there!</div>}
+
+// Take the react component and shot it on the screen
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
 
 
